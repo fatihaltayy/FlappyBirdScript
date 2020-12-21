1ab86600615c431b69143207886e635b9bc203de
Fatih Altay <sayinaltay@gmail.com>
2020-12-21 11:30:35 +0300

    Flappy Bird - Unity

---
+++ Assets/Script/Birdy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdy : MonoBehaviour
+{
+    public bool isDead;
+    public float velocity = 1f;
+    public Rigidbody2D rd2D;
+    public GameManager managerGame;
+    public GameObject DeathScreen;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    void Update()
+    {  
+         //Tıklamayı Al
+       if(Input.GetMouseButtonDown(0)) 
+       {
+           // Havada Kuşu Sıcrat
+           rd2D.velocity = Vector2.up * velocity;
+       }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    
+    {
+        
+        if(collision.gameObject.name == "ScoreArea")
+        {
+            managerGame.UpdateScore();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag =="DeathArea")
+        {
+            isDead = true;
+            Time.timeScale = 0;
+            
+            DeathScreen.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Script/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    private void Start() 
+    {
+        Destroy(gameObject,4);
+    }
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
